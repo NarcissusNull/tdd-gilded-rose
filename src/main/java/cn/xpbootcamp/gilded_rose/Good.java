781f294f05aec20f54e487232ba8dedc69bdc3cc
xiang.li2 <xiang.li2@thoughtworks.com>
2020-03-23 10:54:07 +0800

    使用min方法重构if条件

--- src/main/java/cn/xpbootcamp/gilded_rose/Good.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Good.java
@@ -13,10 +13,7 @@ public class Good {
     }
 
     public void setQuality(int quality) {
-        this.quality = Math.max(quality, 0);
-        if (this.quality > 50) {
-            this.quality = 50;
-        }
+        this.quality = Math.min(Math.max(quality, 0), 50);
     }
 
 }
