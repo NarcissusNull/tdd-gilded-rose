a71df92da196ceb1977ad2cf852a4c8b732b8f98
xiang.li2 <xiang.li2@thoughtworks.com>
2020-03-23 10:46:06 +0800

    完成测试：商品的价值永远不会小于0

--- src/main/java/cn/xpbootcamp/gilded_rose/Good.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/Good.java
@@ -14,6 +14,9 @@ public class Good {
 
     public void setQuality(int quality) {
         this.quality = quality;
+        if (this.quality < 0) {
+            this.quality = 0;
+        }
     }
 
 }
