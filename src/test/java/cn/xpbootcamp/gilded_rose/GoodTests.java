f36addac51edb5ba6c10f329a23f70feb399041c
xiang.li2 <xiang.li2@thoughtworks.com>
2020-03-23 10:49:49 +0800

    新增测试：商品的价值永远不会超过50

--- src/test/java/cn/xpbootcamp/gilded_rose/GoodTests.java
+++ src/test/java/cn/xpbootcamp/gilded_rose/GoodTests.java
@@ -18,4 +18,18 @@ public class GoodTests {
         // then
         assertEquals(0, good.getQuality());
     }
+
+    @Test
+    void should_set_quality_50_when_set_quality_given_quality_more_than_50() {
+        // given
+        int quality = 60;
+        Good good = new Good(10);
+
+        // when
+        good.setQuality(quality);
+
+        // then
+        assertEquals(50, good.getQuality());
+    }
+
 }
